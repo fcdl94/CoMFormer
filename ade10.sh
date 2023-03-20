fa85b567ea5644c56966c8967283d99166495429
fcdl94 <S203813s3>
2023-03-20 16:20:08 +0100

    Starting to clear repo.

--- ade10.sh
+++ ade10.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=4:mem=16GB:ngpus=2
+#PBS -l select=1:ncpus=4:mem=16GB:ngpus=4
 #PBS -l walltime=24:00:00
 #PBS -j oe
 #PBS -N ADE-S10
@@ -13,38 +13,38 @@ cd /work/fcermelli/fcdl/Mask2Former
 
 port=$(python get_free_port.py)
 
-cfg_file=configs/ade20k/semantic-segmentation/maskformer2_R101_bs16_90k.yaml
+export WANDB_API_KEY="e8c87155059d91add34a5ace9e134b018e521377"
+cfg_file=configs/ade20k/semantic-segmentation/swin/maskformer2_swin_small_bs16_160k.yaml # maskformer2_R101_bs16_90k.yaml
 base=ade_ss
 # MODEL.MASK_FORMER.SOFTMASK True MODEL.MASK_FORMER.DICE_WEIGHT 1. MODEL.MASK_FORMER.CLASS_WEIGHT 1. MODEL.MASK_FORMER.MASK_WEIGHT 0.0 MODEL.MASK_FORMER.FOCAL True
 cont_args="CONT.BASE_CLS 100 CONT.INC_CLS 10 CONT.MODE overlap SEED 42"
 task=mya_100-50-ov
 
-name=PerPixel
-meth_args="MODEL.MASK_FORMER.TEST.MASK_BG True MODEL.MASK_FORMER.PER_PIXEL True"
+#name=PerPixel
+#meth_args="MODEL.MASK_FORMER.TEST.MASK_BG True MODEL.MASK_FORMER.PER_PIXEL True"
 #name=MF
 #meth_args="MODEL.MASK_FORMER.TEST.MASK_BG False MODEL.MASK_FORMER.PER_PIXEL False"
-#name=MxF
-#meth_args="MODEL.MASK_FORMER.TEST.MASK_BG False MODEL.MASK_FORMER.PER_PIXEL False MODEL.MASK_FORMER.SOFTMASK True MODEL.MASK_FORMER.FOCAL True"
+name=Swin_MxF
+meth_args="MODEL.MASK_FORMER.TEST.MASK_BG False MODEL.MASK_FORMER.PER_PIXEL False MODEL.MASK_FORMER.SOFTMASK True MODEL.MASK_FORMER.FOCAL True"
 
-comm_args="--dist-url tcp://127.0.0.1:${port} OUTPUT_DIR ${base} ${meth_args} ${cont_args} WANDB False"
+comm_args="--dist-url tcp://127.0.0.1:${port} OUTPUT_DIR ${base} ${meth_args} ${cont_args}" #  WANDB False
 inc_args="CONT.TASK 1 CONT.WEIGHTS ${base}/${task}/${name}/step0/model_final.pth SOLVER.MAX_ITER 4000 SOLVER.BASE_LR 0.00005"
 
 #python train_inc.py --resume --num-gpus 2 --config-file ${cfg_file} ${comm_args} CONT.TASK 0 NAME ${name}
 
 #python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_MIB_500 CONT.DIST.KD_WEIGHT 500. CONT.DIST.UKD True CONT.DIST.UCE True
 #python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_T2_UKD50 CONT.DIST.PSEUDO True CONT.DIST.PSEUDO_TYPE 1 CONT.DIST.KD_WEIGHT 50.0 CONT.DIST.UKD True
-#python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_T2_KD50 CONT.DIST.PSEUDO True CONT.DIST.PSEUDO_TYPE 1 CONT.DIST.KD_WEIGHT 50.0
 #python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_t09 CONT.DIST.PSEUDO True # CONT.DIST.POD_WEIGHT 0.0001
-python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_t08 CONT.DIST.PSEUDO True
-python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_t08_POD CONT.DIST.PSEUDO True CONT.DIST.POD_WEIGHT 0.0001
+
+#python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_T2_UKD1Rew CONT.DIST.PSEUDO True CONT.DIST.PSEUDO_TYPE 1 CONT.DIST.KD_WEIGHT 1. CONT.DIST.UKD True CONT.DIST.KD_REW True
+#python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_T2_UKD50Rew CONT.DIST.PSEUDO True CONT.DIST.PSEUDO_TYPE 1 CONT.DIST.KD_WEIGHT 50. CONT.DIST.UKD True CONT.DIST.KD_REW True
+python train_inc.py --num-gpus 4 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_T2_UKD10Rew CONT.DIST.PSEUDO True CONT.DIST.PSEUDO_TYPE 1 CONT.DIST.KD_WEIGHT 10. CONT.DIST.UKD True CONT.DIST.KD_REW True
 
 
 for t in 2 3 4 5; do
   inc_args="CONT.TASK ${t} SOLVER.MAX_ITER 4000 SOLVER.BASE_LR 0.00005"
-#  python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_MIB_500 CONT.DIST.KD_WEIGHT 500. CONT.DIST.UKD True CONT.DIST.UCE True
-#  python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_T2_UKD50 CONT.DIST.PSEUDO True CONT.DIST.PSEUDO_TYPE 1 CONT.DIST.KD_WEIGHT 50.0 CONT.DIST.UKD True
-#  python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_T2_KD50 CONT.DIST.PSEUDO True CONT.DIST.PSEUDO_TYPE 1 CONT.DIST.KD_WEIGHT 50.0
-  python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_t08 CONT.DIST.PSEUDO True
-  python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_t08_POD CONT.DIST.PSEUDO True CONT.DIST.POD_WEIGHT 0.0001
+#  python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_T2_UKD1Rew CONT.DIST.PSEUDO True CONT.DIST.PSEUDO_TYPE 1 CONT.DIST.KD_WEIGHT 1. CONT.DIST.UKD True CONT.DIST.KD_REW True
+#  python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_T2_UKD50Rew CONT.DIST.PSEUDO True CONT.DIST.PSEUDO_TYPE 1 CONT.DIST.KD_WEIGHT 50. CONT.DIST.UKD True CONT.DIST.KD_REW True
+  python train_inc.py --num-gpus 4 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_T2_UKD10Rew CONT.DIST.PSEUDO True CONT.DIST.PSEUDO_TYPE 1 CONT.DIST.KD_WEIGHT 10. CONT.DIST.UKD True CONT.DIST.KD_REW True
 done
 
--- ade5.sh
+++ ade5.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=4:mem=16GB:ngpus=2
+#PBS -l select=1:ncpus=4:mem=16GB:ngpus=4
 #PBS -l walltime=24:00:00
 #PBS -j oe
 #PBS -N ADE-S5
 #PBS -q gpu
+
 
 module load anaconda/3.2020.2
 module load devtoolset-7/gcc-7.3.1
@@ -13,30 +14,29 @@ cd /work/fcermelli/fcdl/Mask2Former
 
 port=$(python get_free_port.py)
 
-cfg_file=configs/ade20k/semantic-segmentation/maskformer2_R101_bs16_90k.yaml
+#cfg_file=configs/ade20k/semantic-segmentation/maskformer2_R101_bs16_90k.yaml
+cfg_file=configs/ade20k/semantic-segmentation/swin/maskformer2_swin_small_bs16_160k.yaml # maskformer2_R101_bs16_90k.yaml
 base=ade_ss
-# MODEL.MASK_FORMER.SOFTMASK True MODEL.MASK_FORMER.DICE_WEIGHT 1. MODEL.MASK_FORMER.CLASS_WEIGHT 1. MODEL.MASK_FORMER.MASK_WEIGHT 0.0 MODEL.MASK_FORMER.FOCAL True
+
 cont_args="CONT.BASE_CLS 100 CONT.INC_CLS 5 CONT.MODE overlap SEED 42"
 task=mya_100-50-ov
 
-name=PerPixel
-meth_args="MODEL.MASK_FORMER.TEST.MASK_BG True MODEL.MASK_FORMER.PER_PIXEL True"
+#name=PerPixel
+#meth_args="MODEL.MASK_FORMER.TEST.MASK_BG True MODEL.MASK_FORMER.PER_PIXEL True"
 #name=MF
 #meth_args="MODEL.MASK_FORMER.TEST.MASK_BG False MODEL.MASK_FORMER.PER_PIXEL False"
 #name=MxF
-#meth_args="MODEL.MASK_FORMER.TEST.MASK_BG False MODEL.MASK_FORMER.PER_PIXEL False MODEL.MASK_FORMER.SOFTMASK True MODEL.MASK_FORMER.FOCAL True"
+name=Swin_MxF
+meth_args="MODEL.MASK_FORMER.TEST.MASK_BG False MODEL.MASK_FORMER.PER_PIXEL False MODEL.MASK_FORMER.SOFTMASK True MODEL.MASK_FORMER.FOCAL True"
 
 comm_args="--dist-url tcp://127.0.0.1:${port} OUTPUT_DIR ${base} ${meth_args} ${cont_args} WANDB False"
 inc_args="CONT.TASK 1 CONT.WEIGHTS ${base}/${task}/${name}/step0/model_final.pth SOLVER.MAX_ITER 2000 SOLVER.BASE_LR 0.00005"
 
-#python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_T2_UKD10Rew CONT.DIST.PSEUDO True CONT.DIST.PSEUDO_TYPE 1 CONT.DIST.KD_WEIGHT 10. CONT.DIST.UKD True CONT.DIST.KD_REW True
-#python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_KD_500 CONT.DIST.KD_WEIGHT 500.
-python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_t08 CONT.DIST.PSEUDO True
-python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_t08_POD CONT.DIST.PSEUDO True CONT.DIST.POD_WEIGHT 0.0001
+python train_inc.py --num-gpus 4 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_T2_UKD10Rew CONT.DIST.PSEUDO True CONT.DIST.PSEUDO_TYPE 1 CONT.DIST.KD_WEIGHT 10. CONT.DIST.UKD True CONT.DIST.KD_REW True
+
 
 for t in 2 3 4 5 6 7 8 9 10; do
   inc_args="CONT.TASK ${t} SOLVER.MAX_ITER 2000 SOLVER.BASE_LR 0.00005"
-#  python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_T2_UKD10Rew CONT.DIST.PSEUDO True CONT.DIST.PSEUDO_TYPE 1 CONT.DIST.KD_WEIGHT 10. CONT.DIST.UKD True CONT.DIST.KD_REW True
-python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_t08 CONT.DIST.PSEUDO True
-python train_inc.py --num-gpus 2 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_t08_POD CONT.DIST.PSEUDO True CONT.DIST.POD_WEIGHT 0.0001
+  python train_inc.py --num-gpus 4 --config-file ${cfg_file} ${comm_args} ${inc_args} NAME ${name}_PSEUDO_T2_UKD10Rew CONT.DIST.PSEUDO True CONT.DIST.PSEUDO_TYPE 1 CONT.DIST.KD_WEIGHT 10. CONT.DIST.UKD True CONT.DIST.KD_REW True
 done
+
